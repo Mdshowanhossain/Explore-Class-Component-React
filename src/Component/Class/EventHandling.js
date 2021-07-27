a7eb9f451ffa85cde4a2de8b2a32d22db5aea0a0
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-27 06:30:21 +0600

    Explore-Class-Components

--- src/Component/Class/EventHandling.js
+++ src/Component/Class/EventHandling.js
@@ -8,18 +8,24 @@ export default class EventHandling extends Component {
             userName: ' '
         }
     }
+
     handleChange = (e) => {
 
         let myName = e.target.name;
         let myValue = e.target.value;
         this.setState({ [myName]: myValue })
     }
+
+    handleSubmit = () => {
+        alert(this.state.userName);
+    }
+
     render() {
         return (
             <div>
                 <h1>My First Form</h1><br />
                 <h2>{this.state.userName}</h2>
-                <form action="">
+                <form onSubmit={this.handleSubmit}>
                     <input name="userName" onChange={this.handleChange} type="text" />
                     <input type="submit" value="Submit" />
                 </form>

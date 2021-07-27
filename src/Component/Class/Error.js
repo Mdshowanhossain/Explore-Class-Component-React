6466d22a8a53a33da3cc73ea7d2b9d982720fc59
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-27 23:51:21 +0600

    Explore-Class-Component

--- src/Component/Class/Error.js
+++ src/Component/Class/Error.js
@@ -16,25 +16,25 @@ export default class SelectOption extends Component {
         };
     }
 
+
     onChangeHandler = (e) => {
         let selectedCity = e.target.value;
-
         this.setState({ showCity: selectedCity, autoSelect: selectedCity });
+        // console.log(selectedCity);
     }
     render() {
         return (
             <div><br /><br />
                 <select onChange={this.onChangeHandler} value={this.state.autoSelect} >
-                    <option>{this.state.city1}</option>
-                    <option>{this.state.city2}</option>
-                    <option>{this.state.city3}</option>
-                    <option>{this.state.city4}</option>
-                    <option>{this.state.city5}</option>
-                    <option>{this.state.city6}</option>
+                    <h1>{this.state.showCity}</h1>
+                    <option value="">{this.state.city1}</option>
+                    <option value="">{this.state.city2}</option>
+                    <option value="">{this.state.city3}</option>
+                    <option value="">{this.state.city4}</option>
+                    <option value="">{this.state.city5}</option>
+                    <option value="">{this.state.city6}</option>
                 </select>
-                <h1>{this.state.showCity}</h1>
             </div>
         )
     }
 }
-// https://www.youtube.com/watch?v=qZno3Yk8QKc

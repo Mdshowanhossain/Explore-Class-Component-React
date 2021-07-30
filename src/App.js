21db35539cbef1b5250c16daeb59d8c4c7b1fc31
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-07-31 00:59:14 +0600

    Explore-Class-Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import './App.css';
-// import Function1 from './Component/Function/Function1';
-// import Class1 from './Component/Class/Class1';
-// import ButtonClick from './Component/Class/ButtonClick';
-// import ButtonClickF from './Component/Function/ButtonClickF';
-// import State from './Component/Class/State';
-// import State2 from './Component/Class/State2';
-// import State3 from './Component/Class/State3';
-// import State4 from './Component/Class/State4';
-// import Condition from './Component/Class/Condition';
-// import Condition2 from './Component/Class/Condition2';
-// import Refresh from './Component/Class/Refresh';
-// import ReactDomRender from './Component/Class/ReactDomRender';
-// import ReactHydreteMethod from './Component/Class/ReactHydreteMethod';
-// import FindDomNode from './Component/Class/FindDomNode';
-// import SingleInputForm from './Component/Class/SingleInputForm';
-// import EventHandling from './Component/Class/EventHandling';
-// import MultipleInputs from './Component/Class/MultipleInputs';
-// import FormValidation from './Component/Class/FormValidation';
-// import TextArea from './Component/Class/TextArea';
-// import SelectOption from './Component/Class/SelectOption';
-// import ListItem from './Component/Class/ListItem';
-// import JsonListItem from './Component/Class/JsonListItem';
-// import ReactRouter from './Component/Class/ReactRouter';
-// import CssParent from './Component/CssStyles/CssParent';
-// import Sass from './Component/SASS/Sass';
-// import GetData from './Component/LoadData/GetData';
-// import PostData from './Component/LoadData/PostData';
-// import Table from './Component/ReactTable/Table';
+import Function1 from './Component/Function/Function1';
+import Class1 from './Component/Class/Class1';
+import ButtonClick from './Component/Class/ButtonClick';
+import ButtonClickF from './Component/Function/ButtonClickF';
+import State from './Component/Class/State';
+import State2 from './Component/Class/State2';
+import State3 from './Component/Class/State3';
+import State4 from './Component/Class/State4';
+import Condition from './Component/Class/Condition';
+import Condition2 from './Component/Class/Condition2';
+import Refresh from './Component/Class/Refresh';
+import ReactDomRender from './Component/Class/ReactDomRender';
+import ReactHydreteMethod from './Component/Class/ReactHydreteMethod';
+import FindDomNode from './Component/Class/FindDomNode';
+import SingleInputForm from './Component/Class/SingleInputForm';
+import EventHandling from './Component/Class/EventHandling';
+import MultipleInputs from './Component/Class/MultipleInputs';
+import FormValidation from './Component/Class/FormValidation';
+import TextArea from './Component/Class/TextArea';
+import SelectOption from './Component/Class/SelectOption';
+import ListItem from './Component/Class/ListItem';
+import JsonListItem from './Component/Class/JsonListItem';
+import ReactRouter from './Component/Class/ReactRouter';
+import CssParent from './Component/CssStyles/CssParent';
+import Sass from './Component/SASS/Sass';
+import GetData from './Component/LoadData/GetData';
+import PostData from './Component/LoadData/PostData';
+import Table from './Component/ReactTable/Table';
+import UseState from './Hook/UseState';
+import Home from './MyContext/Home';
+import { OurProvider } from './MyContext/MyContext';
+import About from './Props/About';
+import Contact from './Props/Contact';
+// import Home from './Props/Home';
 
 
 
@@ -96,6 +102,15 @@ function App() {
       {/* <ParentNav /> */}
       {/* <ParentRoute /> */}
       {/* </BrowserRouter> */}
+
+
+      {/* <UseState /> */}
+
+      <Home />value="-- I am in PassBy Props" />
+      <About />
+      <Contact value="--I am in By Props." />
+
+
 
     </div>
   );

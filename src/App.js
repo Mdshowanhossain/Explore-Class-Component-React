f5ce1c3525267858e3fad761fc1ce81e60e803ec
Mdshowanhossain <76901439+Mdshowanhossain@users.noreply.github.com>
2021-08-11 00:45:35 +0600

    Explore-Class-Component-React

--- src/App.js
+++ src/App.js
@@ -1,119 +1,50 @@
-import './App.css';
-import Function1 from './Component/Function/Function1';
-import Class1 from './Component/Class/Class1';
-import ButtonClick from './Component/Class/ButtonClick';
-import ButtonClickF from './Component/Function/ButtonClickF';
-import State from './Component/Class/State';
-import State2 from './Component/Class/State2';
-import State3 from './Component/Class/State3';
-import State4 from './Component/Class/State4';
-import Condition from './Component/Class/Condition';
-import Condition2 from './Component/Class/Condition2';
-import Refresh from './Component/Class/Refresh';
-import ReactDomRender from './Component/Class/ReactDomRender';
-import ReactHydreteMethod from './Component/Class/ReactHydreteMethod';
-import FindDomNode from './Component/Class/FindDomNode';
-import SingleInputForm from './Component/Class/SingleInputForm';
-import EventHandling from './Component/Class/EventHandling';
-import MultipleInputs from './Component/Class/MultipleInputs';
-import FormValidation from './Component/Class/FormValidation';
-import TextArea from './Component/Class/TextArea';
-import SelectOption from './Component/Class/SelectOption';
-import ListItem from './Component/Class/ListItem';
-import JsonListItem from './Component/Class/JsonListItem';
-import ReactRouter from './Component/Class/ReactRouter';
-import CssParent from './Component/CssStyles/CssParent';
-import Sass from './Component/SASS/Sass';
-import GetData from './Component/LoadData/GetData';
-import PostData from './Component/LoadData/PostData';
-import Table from './Component/ReactTable/Table';
-import UseState from './Hook/UseState';
-import Home from './MyContext/Home';
-import { OurProvider } from './MyContext/MyContext';
-import About from './Props/About';
-import Contact from './Props/Contact';
-// import Home from './Props/Home';
+import React from 'react';
+import FormHandle from './Components/Formahandle/FormHandle';
+import Clock from './Components/StateLifeCycle/Clock/Clock';
+import ClockList from './Components/StateLifeCycle/Clock/ClockList';
+import CalculatorBoilWater from './Components/LiftingStateUp/CalculatorBoilWater';
+import ButtonClicked from './Components/HOC/ButtonClicked';
+import HeadingHover from './Components/HOC/HeadingHover';
+import Counter from './Components/RenderProps/Counter';
+import HoverCounter from './Components/RenderProps/HoverCounter';
+import User from './Components/RenderProps/User';
+import ClickCounter from './Components/RenderProps/ClickCounter';
 
+const App = () => {
 
+  // let quantities = [1, 2, 3, 4];
 
-
-
-
-function App() {
   return (
-    <div className="App">
-      {/* <Function1 name="Sohan" age="20" />
-      <Function1 name="Osman" age="20" />
-      <ButtonClickF /><br />
+    <div>
 
-      <Class1 name="Class-5" age="44" />
-      <Class1 name="Class-6" age="45" />
-      <ButtonClick /> */}
+      {/* <-----------------STATE LIFECYCLE & CONDITIONAL RENDERING-----------------------> */}
+      {/* <Clock /> */}
+      {/* <ClockList quantities={quantities} /> */}
+      {/* <-----------------STATE LIFECYCLE & CONDITIONAL RENDERING END-----------------------> */}
 
-      {/* <State /> */}
+      {/* <-----------------FORM HANDLING-----------------------> */}
+      {/* <FormHandle />   */}
+      {/* <-----------------FORM HANDLING END-----------------------> */}
 
-      {/* <State2 /> */}
+      {/* <-----------------LIFTING STATE UP-----------------------> */}
+      {/* <CalculatorBoilWater /> */}
+      {/* <-----------------LIFTING STATE UP END-----------------------> */}
 
-      {/* <State3 /> */}
+      {/* <-----------------HIGHER ORDER FUNCTION-----------------------> */}
+      {/* <ButtonClicked />
+      <HeadingHover /> */}
+      {/* <-----------------HIGHER ORDER FUNCTION END-----------------------> */}
 
-      {/* <State4 /> */}
+      {/* <-----------------RENDER PROPS----------------------->*/}
 
-      {/* <Condition /> */}
+      {/* <User render={(isTrue) => isTrue ? 'sohan' : `Don't know You`} /> */}
 
-      {/* <Condition2 /> */}
+      <Counter render={(counter, handleCounter) => <ClickCounter counter={counter} handleCounter={handleCounter} />} />
+      <Counter render={(counter, handleCounter) => <HoverCounter counter={counter} handleCounter={handleCounter} />} />
 
-      {/* <Refresh /> */}
-
-      {/* <ReactDomRender /> */}
-
-      {/* <ReactHydreteMethod /> */}
-
-      {/* <FindDomNode /> */}
-
-      {/* <SingleInputForm /> */}
-
-      {/* <EventHandling /> */}
-
-      {/* <MultipleInputs /> */}
-
-      {/* <FormValidation /> */}
-
-      {/* <TextArea /> */}
-
-      {/* <SelectOption /> */}
-
-      {/* <ListItem /> */}
-
-      {/* <JsonListItem /> */}
-
-      {/* <ReactRouter /> */}
-
-      {/* <CssParent /> */}
-
-      {/* <Sass /> */}
-
-      {/* <GetData /> */}
-
-      {/* <PostData /> */}
-
-      {/* <Table /> */}
-
-      {/* <BrowserRouter> */}
-      {/* <ParentNav /> */}
-      {/* <ParentRoute /> */}
-      {/* </BrowserRouter> */}
-
-
-      {/* <UseState /> */}
-
-      {/* <Home />value="-- I am in PassBy Props" />
-      <About />
-      <Contact value="--I am in By Props." /> */}
-
-
-
+      {/* <-----------------RENDER PROPS END----------------------->*/}
     </div>
   );
-}
+};
 
 export default App;
